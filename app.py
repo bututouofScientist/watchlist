d308920990ce4970ed0c6f0f3c210b613596066c
by <690764925@qq.com>
2021-03-28 16:31:16 +0800

    Add index page using templates

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
-from flask import Flask,url_for
+from flask import Flask, url_for,render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Hello'
+def index():
+    return render_template("index.html", name=name, movies=movies)
 
 
 @app.route("/user/<name>")
 def user_page(name):
     return "User:%s" % name
+
+
+name = 'Boyu'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 
 @app.route('/test')
